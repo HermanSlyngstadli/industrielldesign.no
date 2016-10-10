f7931e873895b318400687c49589dabe69850a5b
Herman <herman@slyngstadli.no>
2016-10-10 10:26:53 +0200

    Enda en liten klargjøring

--- js/importHead.js
+++ js/importHead.js
@@ -3,12 +3,12 @@
 // 		by Herman Slyngstadli
 // 
 
-var head = document.getElementByTagName('head');
+var head = document.head;
 
 var fyll;
 
 // Adding each element one by one for easier editing
-fyll += '<link href="https://fonts.googleapis.com/css?family=Open+Sans|Raleway" rel="stylesheet">';
+fyll = '<link href="https://fonts.googleapis.com/css?family=Open+Sans|Raleway" rel="stylesheet">';
 fyll += '<link rel="stylesheet" href="css/style.css">';
 
-head.innerHTML = fyll;
+head.innerHTML += fyll;
--- js/importHeader.js
+++ js/importHeader.js
@@ -5,4 +5,4 @@
 
 var header = document.getElementById('header');
 
-header.innerHTML = '<div class="header"><ul> <li><a href="">Prosjekter</a></li> <li><a href="">Student</a></li> <li><a href="">Bedrift</a></li> <li><a href="">Om Leonardo</a></li> </ul></div>';
+header.innerHTML = '<div class="header"><ul> <li><a href="prosjekter.html">Prosjekter</a></li> <li><a href="student.html">Student</a></li> <li><a href="business.html">Bedrift</a></li> <li><a href="aboutLeo.html">Om Leonardo</a></li> </ul></div>';
