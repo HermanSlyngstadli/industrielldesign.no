601fcc3296cbd251d00ce26458dda0447e1b1e91
TobWul <tobias.wulvik@gmail.com>
2016-10-10 11:54:25 +0200

    La inn extrabold i head.js

--- js/importHead.js
+++ js/importHead.js
@@ -8,7 +8,7 @@ var head = document.head;
 var fyll;
 
 // Adding each element one by one for easier editing
-fyll = '<link href="https://fonts.googleapis.com/css?family=Open+Sans|Raleway" rel="stylesheet">';
+fyll = '<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,800|Raleway" rel="stylesheet">';
 fyll += '<link rel="stylesheet" href="css/style.css">';
 
 head.innerHTML += fyll;
