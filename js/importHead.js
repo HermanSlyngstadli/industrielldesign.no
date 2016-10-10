e86faedeecc4cea22c29c6ddc493467f7f4e0e5e
TobWul <tobias.wulvik@gmail.com>
2016-10-10 12:35:09 +0200

    La inn flere meta-tager i importHead.js

--- js/importHead.js
+++ js/importHead.js
@@ -10,5 +10,7 @@ var fyll;
 // Adding each element one by one for easier editing
 fyll = '<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,800|Raleway" rel="stylesheet">';
 fyll += '<link rel="stylesheet" href="css/style.css">';
+fyll += '<meta charset="utf-8">';
+fyll += '<meta lang="no">';
 
 head.innerHTML += fyll;
