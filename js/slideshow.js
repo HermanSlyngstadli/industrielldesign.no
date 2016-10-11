f9ca9fb9f4ff4d9e6933830c2026dd70067fd09b
Herman <herman@slyngstadli.no>
2016-10-11 23:20:11 +0200

    Starta på slideshow.js og nytt css-doc, endra litt i style.css. Fiksa videre på student.html

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,4 +1,79 @@
 //
-//		Slideshow.js
+//		Slideshow.js aka. Crayslider (So cray ammagad)
 //		by Herman Slyngstadli
 //
+
+
+// Init
+var sliderWrapperId = 'eventSlider';
+var sliderId = 'eventSlider';
+var focusedSlide = 'focusedEvent';
+var slidesOnEachPage = 3; // Slides on each page
+
+var sliderWrapper = document.getElementById(sliderWrapperId);
+var slider = document.getElementById(sliderId);
+
+var slides;
+var currentSlide = 1;
+
+function happening(image, title, text, date, time, location) {
+	this.image = image;
+	this.title = title;
+	this.text = text;
+	this.date = date;
+	this.time = time;
+	this.location = location;
+};
+
+var slide1 = new happening('','Halloween','Det er halloweenfest på huset i år også!','29.10','20:00','IPD');
+var slide2 = new happening('','Gravøl','Det er Gravøl på huset i år også!','03.02','17:00','IPD');
+var slide3 = new happening('','Festfest','Det er festfest på huset i år også!','09.03','19:00','IPD');
+var slide4 = new happening('','Halloween','Det er halloweenfest på huset i år også!','29.10','20:00','IPD');
+
+slides = [slide1, slide2, slide3, slide4];
+
+// Create a sliderWrapper
+var craysliderWrapper = document.createElement('ul');
+craysliderWrapper.className='craysliderWrapper';
+
+// Adjust the size of the slides and the slidewrapper
+craysliderWrapper.style.width = 100*slides.length + '%';
+
+// Make slides and append them to the DOM
+for (var i = 0; i < slides.length; i++) {
+	// Create list element
+	var craysliderElement = document.createElement('li');
+	craysliderElement.className = 'craysliderElement';
+	// Create a header for the element
+	var craysliderElementHeader = document.createElement('h4');
+	craysliderElementHeader.className = 'craysliderElementHeader';
+	// Create a wrapper for the description
+	var craysliderElementText = document.createElement('div');
+	craysliderElementText.className = 'craysliderElementText';
+
+	var crayElementHeader = document.createTextNode(slides[i].title);
+	var crayElementText = document.createTextNode(slides[i].text);
+	
+
+	craysliderElementHeader.appendChild(crayElementHeader);
+	craysliderElementText.appendChild(crayElementText);
+	
+	craysliderElement.appendChild(craysliderElementHeader);
+	craysliderElement.appendChild(craysliderElementText);
+
+	// Set the width of the slides
+	craysliderElement.style.width = 100/(slides.length*slidesOnEachPage) + '%';
+
+	craysliderWrapper.appendChild(craysliderElement);
+}
+slider.appendChild(craysliderWrapper);
+
+function pushIt(direction) {
+	if(direction === 'left' && currentSlide > 1) {
+		craysliderWrapper.style.left = 100*(currentSlide-2) + '%';
+		currentSlide--;
+	} else if(direction === 'right' && currentSlide < Math.ceil(slides.length/slidesOnEachPage)) {
+		craysliderWrapper.style.left = -100*currentSlide + '%';
+		currentSlide++;
+	}
+}
