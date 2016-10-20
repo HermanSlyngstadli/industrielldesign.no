0fea3ab347468f99cea20c4e11afd7e58752a8e2
TobWul <tobias.wulvik@gmail.com>
2016-10-20 21:53:37 +0200

    Den satans kuknissejæveln av en xml vil jo fan ikke funk... -.-

--- js/displayArticle.js
+++ js/displayArticle.js
@@ -2,3 +2,31 @@
 //		displayArticle.js
 //		by Tobias Wulvik
 //
+
+// Laster inn tagene til variabler
+var paragraph = document.getElementById("articleP");
+var title = document.getElementsByClassName("sectionTitle")[0];
+console.log(title);
+var image = document.getElementById("articlePhoto");
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        updateArticle(this);
+    }
+};
+xhttp.open("GET", "wikiArtikler.xml", true);
+xhttp.send();
+
+function updateArticle(xml) {
+    var xmlDoc = xml.responseXML;
+    var x;
+    var txt = "";
+    x = xmlDoc.getElementsByTagName("title")[0].childNodes[0];
+    txt += x.nodeValue + "<br>";
+    x.nodeValue = "Easy Cooking";
+    x = xmlDoc.getElementsByTagName("title")[0].childNodes[0];
+    txt += x.nodeValue + "<br>";
+    paragrapg = txt;
+}
+
