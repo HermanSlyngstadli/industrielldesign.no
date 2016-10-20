6b47035257795e398d900474f6845cbff067a20b
Herman <herman@slyngstadli.no>
2016-10-20 15:27:03 +0200

    Samme

---
+++ js/importLogo.js
@@ -0,0 +1,18 @@
+//
+//	Import Logo
+//		by Herman Slyngstadli
+//
+
+var logo = document.getElementById('logo');
+
+logo.innerHTML = ''
+			+ '<div id="logoWrapper">'
+				+ '<div id="logoBackground">'
+					+ '<div id="elHorisontal">'
+					+ '</div>'
+					+ '<div id="elVertical">'
+					+ '</div>'
+					+ '<div id="elDot">'
+					+ '</div>'
+				+ '</div>'
+			+ '</div>';
