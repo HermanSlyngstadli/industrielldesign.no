1992fc123a35a051dd7b722525c27ad9b59e6f08
Bakli1 <ragnhildbakli@hotmail.com>
2016-10-24 11:27:27 +0200

    Trekkspillmeny og litt stæsj i student.html til menyen

--- js/accordianmenu.js
+++ js/accordianmenu.js
@@ -2,3 +2,13 @@
 //		Accordianmenu.js
 //		by Ragnhild Bakli
 //
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+  }
+}
