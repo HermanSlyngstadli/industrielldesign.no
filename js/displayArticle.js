7953d989a9f77a760a0bce10df4baca5a74c6ee4
TobWul <tobias.wulvik@gmail.com>
2016-10-24 10:13:19 +0200

    dsad

--- js/displayArticle.js
+++ js/displayArticle.js
@@ -6,27 +6,19 @@
 // Laster inn tagene til variabler
 var paragraph = document.getElementById("articleP");
 var title = document.getElementsByClassName("sectionTitle")[0];
-console.log(title);
 var image = document.getElementById("articlePhoto");
+console.log(image)
 
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        updateArticle(this);
-    }
-};
-xhttp.open("GET", "wikiArtikler.xml", true);
-xhttp.send();
+function readXML() {
 
-function updateArticle(xml) {
-    var xmlDoc = xml.responseXML;
-    var x;
-    var txt = "";
-    x = xmlDoc.getElementsByTagName("title")[0].childNodes[0];
-    txt += x.nodeValue + "<br>";
-    x.nodeValue = "Easy Cooking";
-    x = xmlDoc.getElementsByTagName("title")[0].childNodes[0];
-    txt += x.nodeValue + "<br>";
-    paragrapg = txt;
+    var xml = new XMLHttpRequest();
+    xml.open("GET", "wikiArtikler.xml", false);
+    xml.send();
+
+    var xmlData = xml.responseText;
+    document.write(xmlData);
+    
+
 }
 
+readXML()
