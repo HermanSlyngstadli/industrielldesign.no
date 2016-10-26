9a7cb76033320f76867a2a31e397210bccd69c54
TobWul <tobias.wulvik@gmail.com>
2016-10-26 16:14:03 +0200

    Har endret noe, usikker på hva

--- js/displayArticle.js
+++ js/displayArticle.js
@@ -3,23 +3,16 @@
 //		by Tobias Wulvik
 //
 
-var artikkel = 0;
 
-/*var xml = new XMLHttpRequest();
-xml.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-        updateArticle(this);
-    }
-};
 
-xml.open("GET", "wikiArtikler.xml", false);
-xml.send();*/
 
-// Array med titlene fra xml-dokumentet
+// Array med titlene fra json-dokumentet
 var alleArtikkelTitler = ["laserkutter", "rød benk", "sag", "styret"];
+console.log(alleArtikkelTitler);
 
-// Array med titlene fra xml-dokumentet
+// Array med titlene fra json-dokumentet
 var alleArtikkelP = ["1Dette er en paragraf", "2Dette er en paragraf", "3Dette er en paragraf", "4Dette er en paragraf"];
+console.log(alleArtikkelP);
 
 // Skal være det faktiske søket
 var sok = "Sag";
