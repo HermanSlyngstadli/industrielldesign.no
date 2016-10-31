44fbeed493b3e8875bf8c0f8279921ac158aefca
Bakli1 <ragnhildbakli@hotmail.com>
2016-10-31 12:30:57 +0100

    mer trekkspill

--- js/accordianmenu.js
+++ js/accordianmenu.js
@@ -12,3 +12,15 @@ for (i = 0; i < acc.length; i++) {
         this.nextElementSibling.classList.toggle("show");
   }
 }
+
+var arr = [{tittel:"R", url:"http://www.vg.no"},{tittel:"A", url:""},
+			{tittel:"G", url:""},{tittel:"N", url:""},
+			{tittel:"H", url:""},{tittel:"I", url:""},
+			{tittel:"L", url:""},{tittel:"D", url:""}]
+			
+for (i=0; i<=arr.length; i++){
+	var fyll = '<a href="' + arr[i].tittel + '">' + arr[i].tittel + '</a>';
+	document.getElementById('verksted').innerHTML += '<li>' + fyll + '</li>';
+	document.getElementById('programmer').innerHTML += '<li>' + fyll + '</li>';
+	document.getElementById('lærere').innerHTML += '<li>' + fyll + '</li>';
+}
