7ffa3775ed701b7991ff8660120e8c299fbcc59a
Herman <herman@slyngstadli.no>
2016-11-03 23:30:59 +0100

    Fixa header

--- js/importHeader.js
+++ js/importHeader.js
@@ -5,4 +5,22 @@
 
 var header = document.getElementById('header');
 
-header.innerHTML = '<div class="header"><a id="logo" href="index.html" style="width:60px;height:60px; padding:0; float:left;"><img src="http://industrielldesign.com/wp-content/uploads/2013/10/leonardo-logo-60x60.png" /></a><ul> <li><a href="prosjekter.html">Prosjekter</a></li> <li><a href="student.html">Student</a></li> <li><a href="business.html">Bedrift</a></li> <li><a href="aboutLeo.html">Om Leonardo</a></li> </ul></div>';
+header.innerHTML = '<div id="headerFixed" class="header">'
+						+'<a id="logo" href="index.html" style="width:60px;height:60px; padding:0; float:left;">'
+							+'<img src="http://industrielldesign.com/wp-content/uploads/2013/10/leonardo-logo-60x60.png" />'
+						+'</a>'
+						+'<ul>'
+							+'<li><a href="prosjekter.html">Prosjekter</a></li>'
+							+'<li><a href="student.html">Student</a></li>'
+							+'<li><a href="business.html">Bedrift</a></li>'
+							+'<li><a href="aboutLeo.html">Om Leonardo</a></li>' 
+						+'</ul>'
+					+'</div>';
+
+window.addEventListener('scroll', function() {
+	if(window.pageYOffset > 100) {
+		document.getElementById('headerFixed').className = 'header headerScrolled';
+	} else if (window.pageYOffset <= 100) {
+		document.getElementById('headerFixed').className = 'header';
+	}
+});
